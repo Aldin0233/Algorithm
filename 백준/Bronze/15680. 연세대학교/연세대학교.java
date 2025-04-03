6b7e79864ad4cd4b99df9637d7bd273e78e842f1
Aldin0233 <160988294+Aldin0233@users.noreply.github.com>
2025-04-04 02:19:46 +0900

    [Bronze V] Title: 연세대학교, Time: 96 ms, Memory: 14004 KB -BaekjoonHub

---
+++ 백준/Bronze/15680. 연세대학교/연세대학교.java
@@ -0,0 +1,16 @@
+import java.io.*;
+import java.util.*;
+
+class Main {
+
+    static int A, B, C;
+    static StringBuilder ans = new StringBuilder();
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+
+        System.out.println(N == 0 ? "YONSEI" : "Leading the Way to the Future");
+    }
+
+}
