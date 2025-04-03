5c9267135f538814685600a14efb299a25c611e6
Aldin0233 <160988294+Aldin0233@users.noreply.github.com>
2025-04-04 02:17:38 +0900

    [Bronze V] Title: 홍익대학교, Time: 172 ms, Memory: 17664 KB -BaekjoonHub

---
+++ 백준/Bronze/16394. 홍익대학교/홍익대학교.java
@@ -0,0 +1,14 @@
+import java.io.*;
+import java.util.*;
+
+class Main {
+
+    static int A, B, C;
+    static StringBuilder ans = new StringBuilder();
+
+    public static void main(String[] args) throws Exception {
+        Scanner sc = new Scanner(System.in);
+        System.out.println(sc.nextInt() - 1946);
+    }
+
+}
