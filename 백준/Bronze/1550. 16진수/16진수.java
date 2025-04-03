21de84e015b48b67753cda01fbe23c1a946ce51f
Aldin0233 <160988294+Aldin0233@users.noreply.github.com>
2025-04-04 00:43:43 +0900

    [Bronze II] Title: 16진수, Time: 100 ms, Memory: 14284 KB -BaekjoonHub

---
+++ 백준/Bronze/1550. 16진수/16진수.java
@@ -0,0 +1,13 @@
+import java.io.*;
+import java.util.*;
+
+class Main {
+
+    static int A, B, C;
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        System.out.println(Integer.parseUnsignedInt(br.readLine(), 16));
+    }
+
+}
