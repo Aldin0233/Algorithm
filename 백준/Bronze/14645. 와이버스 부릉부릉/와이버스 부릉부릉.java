5a13c63e2dfd6cec303f6886f3f13b3f2010a5ae
Aldin0233 <160988294+Aldin0233@users.noreply.github.com>
2025-04-04 02:20:49 +0900

    [Bronze V] Title: 와이버스 부릉부릉, Time: 92 ms, Memory: 13924 KB -BaekjoonHub

---
+++ 백준/Bronze/14645. 와이버스 부릉부릉/와이버스 부릉부릉.java
@@ -0,0 +1,13 @@
+import java.io.*;
+import java.util.*;
+
+class Main {
+
+    static int A, B, C;
+    static StringBuilder ans = new StringBuilder();
+
+    public static void main(String[] args) throws Exception {
+        System.out.println("비와이");
+    }
+
+}
