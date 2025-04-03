99961b31daa7eef5561ce730d8793411ee074099
Aldin0233 <160988294+Aldin0233@users.noreply.github.com>
2025-04-03 22:51:20 +0900

    [Bronze V] Title: 오늘 날짜, Time: 100 ms, Memory: 13988 KB -BaekjoonHub

---
+++ 백준/Bronze/10699. 오늘 날짜/오늘 날짜.java
@@ -0,0 +1,15 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+    static int N, S, ans = 0;
+    static int[] arr;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        System.out.println("2025-04-03");
+    }
+
+}
