4eda575d1ce7fce3cc79b67f75177c217b9c5d46
Aldin0233 <160988294+Aldin0233@users.noreply.github.com>
2025-05-15 22:47:50 +0900

    [Bronze V] Title: 사파리월드, Time: 172 ms, Memory: 17608 KB -BaekjoonHub

---
+++ 백준/Bronze/2420. 사파리월드/사파리월드.java
@@ -0,0 +1,19 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+    static int T, K;
+    static StringBuilder ans = new StringBuilder(); //결과
+
+    public static void main(String[] args) throws IOException {
+        Scanner sc = new Scanner(System.in);
+        long N = sc.nextLong();
+        long M = sc.nextLong();
+        System.out.println(Math.abs(N - M));
+    }
+
+
+
+
+}
