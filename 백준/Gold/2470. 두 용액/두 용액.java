d5ade347f4bb314e46b758e82c57b0975435736d
Aldin0233 <160988294+Aldin0233@users.noreply.github.com>
2025-05-24 22:41:25 +0900

    [Gold V] Title: 두 용액, Time: 312 ms, Memory: 28064 KB -BaekjoonHub

---
+++ 백준/Gold/2470. 두 용액/두 용액.java
@@ -0,0 +1,46 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+    static int N; //용액 갯수
+    static int diff = Integer.MAX_VALUE; //최소 차잇값
+    static int[] arr; //정렬된 용액 목록
+    static int leftAns, rightAns; //출력한 좌측, 우측값(오름차순)
+    static StringBuilder ans = new StringBuilder(); //결과
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        N = Integer.parseInt(br.readLine());
+        arr = new int[N];
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < N; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+
+        Arrays.sort(arr);
+
+        int left = 0, right = N - 1;
+        while(left < right) {
+            int sum = arr[left] + arr[right]; //기본 합
+            int newDiff = Math.abs(sum); //0에 가까운 정도
+            if (newDiff < diff) {
+                leftAns = arr[left];
+                rightAns = arr[right];
+                diff = newDiff;
+            }
+            if(sum > 0) { //sum이 컷으면 알칼리쪽으로
+                right--;
+            } else if(sum < 0) { //산성쪽으로
+                left++;
+            } else { //0이면 탐지 멈춤
+                break;
+            }
+        }
+        System.out.println(ans.append(leftAns).append(" ").append(rightAns));
+    }
+
+
+
+
+}
