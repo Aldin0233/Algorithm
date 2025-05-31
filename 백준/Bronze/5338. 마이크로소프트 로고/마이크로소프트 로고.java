9ad4478b8a1cc214a274610224ab7c16f56bd26c
Aldin0233 <160988294+Aldin0233@users.noreply.github.com>
2025-05-31 23:23:30 +0900

    [Bronze V] Title: 마이크로소프트 로고, Time: 108 ms, Memory: 14072 KB -BaekjoonHub

---
+++ 백준/Bronze/5338. 마이크로소프트 로고/마이크로소프트 로고.java
@@ -0,0 +1,24 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+    static int T, N, M, printCnt;
+    static int[] cntArr;
+    static StringBuilder ans = new StringBuilder();
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        System.out.println("       _.-;;-._\n" +
+                "'-..-'|   ||   |\n" +
+                "'-..-'|_.-;;-._|\n" +
+                "'-..-'|   ||   |\n" +
+                "'-..-'|_.-''-._|");
+    }
+
+
+
+}
+
+
