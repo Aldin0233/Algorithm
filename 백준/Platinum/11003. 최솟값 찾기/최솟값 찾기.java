fc8a3774d6522b685c55e638c0e9b39574c24681
Aldin0233 <160988294+Aldin0233@users.noreply.github.com>
2025-07-07 15:07:56 +0900

    [Platinum V] Title: 최솟값 찾기, Time: 2052 ms, Memory: 565380 KB -BaekjoonHub

--- 백준/Platinum/11003. 최솟값 찾기/최솟값 찾기.java
+++ 백준/Platinum/11003. 최솟값 찾기/최솟값 찾기.java
@@ -4,8 +4,8 @@ import java.util.*;
 public class Main {
 
     static int N, L;
-    static int[] arr, min;
-    static Deque<Integer> dq;
+    static int[] arr, min; //배열, 최솟값
+    static Deque<Integer> dq; //덱을 이용해 슬라이딩 윈도우
     static StringBuilder ans = new StringBuilder();
 
     public static void main(String[] args) throws IOException {
@@ -19,17 +19,19 @@ public class Main {
         st = new StringTokenizer(br.readLine());
         for(int i = 0; i < N; i++) {
             arr[i] = Integer.parseInt(st.nextToken());
+            //L이상 부터 앞부터 순서대로(슬라이딩 윈도우) 제거
             if(!dq.isEmpty() && dq.peekFirst() <= i - L) {
                 dq.pollFirst();
             }
-
+            //뒤에서부터(앞은 주기적으로 제거)
+            //현재 투입되는 값보다 큰 값들은 미리 제거함
             while(!dq.isEmpty() && arr[dq.peekLast()] >= arr[i]) {
                 dq.pollLast();
             }
 
-            dq.addLast(i);
+            dq.addLast(i); //현재 값 추가
 
-            min[i] = arr[dq.peekFirst()];
+            min[i] = arr[dq.peekFirst()]; // 첫번째 값이 현재 가장 낮은 값
         }
 
         for(int i = 0; i < N; i++) {
