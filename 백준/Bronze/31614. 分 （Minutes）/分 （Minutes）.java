ec45a997b8934bdb21c1ae33ce674c625d3cab37
Aldin0233 <160988294+Aldin0233@users.noreply.github.com>
2025-10-16 04:09:24 +0900

    [Bronze V] Title: 分 (Minutes), Time: 104 ms, Memory: 14320 KB -BaekjoonHub

---
+++ 백준/Bronze/31614. 分 （Minutes）/分 （Minutes）.java
@@ -0,0 +1,21 @@
+import java.io.*;
+import java.util.*;
+
+
+public class Main {
+
+    static int H, M;
+    static StringBuilder ans = new StringBuilder();
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        H = Integer.parseInt(br.readLine()); M = Integer.parseInt(br.readLine());
+        System.out.println(H * 60 + M);
+    }
+
+
+
+
+
+}
+
