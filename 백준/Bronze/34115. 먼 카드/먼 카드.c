dc476dcb9a4884b95c39cce468d4acc3508e1657
Aldin0233 <160988294+Aldin0233@users.noreply.github.com>
2026-04-13 22:26:57 +0900

    [Bronze III] Title: 먼 카드, Time: 0 ms, Memory: 1112 KB, Score: 100 point -BaekjoonHub

--- 백준/Bronze/34115. 먼 카드/먼 카드.c
+++ 백준/Bronze/34115. 먼 카드/먼 카드.c
@@ -1,6 +1,10 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+//34115 먼카드
+//모든 카드는 단 두장씩만 들어오기로 약속되어 있다. 
+//따라서 이전에 카드가 들어왔다면, 다음 카드가 들어올 때는 그 사이 거리만 확인하면 된다.
+
 static inline int max(int a, int b) {
     return a > b ? a : b;
 }
@@ -8,19 +12,22 @@ static inline int max(int a, int b) {
 int main() {
     int N;
     scanf("%d", &N);
-    int* arr = calloc(N, sizeof(int));
+    int X[N + 1];
+    for(int i = 1; i <= N; i++) {
+        X[i] = 0;
+    }
     int curMax = 0;
+    //1부터 시작하고 대신 0을 기존에 들어온적 있는지 여부로 판단한다.
     for(int i = 1; i <= N * 2; i++) {
-        int tmp;
-        scanf("%d", &tmp);
-        if(arr[tmp - 1]) {
-            curMax = max(curMax, i - arr[tmp - 1] - 1);
-        } else {
-            arr[tmp - 1] = i;
+        int input;
+        scanf("%d", &input);
+        if(X[input]) {
+            curMax = max(curMax, i - X[input] - 1);
+        } else { // C, C++에서는 0을 거짓으로 취급
+            X[input] = i;
         }
     }
 
     printf("%d", curMax);
-    free(arr);
     return 0;
 }
