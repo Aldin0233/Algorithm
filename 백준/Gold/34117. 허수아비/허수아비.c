93b6b82c3070afecf1e709b490b8fe858192e5bd
Aldin0233 <160988294+Aldin0233@users.noreply.github.com>
2026-04-16 02:46:27 +0900

    [Gold III] Title: 허수아비, Time: 148 ms, Memory: 6856 KB, Score: 150 point -BaekjoonHub

--- 백준/Gold/34117. 허수아비/허수아비.c
+++ 백준/Gold/34117. 허수아비/허수아비.c
@@ -8,58 +8,37 @@
 //화살이 위치 i에서 멈추거나 왼쪽에서 멈추도록 하기 위해 필요한 허수아비의 최소 개수, 
 //멈출 수 없을 때 출력 -1
 //힘이 최대 1억 -> 단순 DP 반복 X
-//우선순위 큐? 높은 거 먼저 나오게 해서 P보다 높아지는 지 확인
-//삽입이 50만이라 우선순위 큐 불가능.//다시 넣어야 하는데 그 과정에서 시간 초과
-//세그 트리를 이용해 내림차순 누적합을 구해보는 느낌으로 가보자.
-//1억개로 세그 트리를 만들면 트리 공간 터지니까 좌표 압축
+//우선순위 큐? 들어오는 대로 추가하고, 만약 P보다 클때 Peek을 빼도 P보다 크다면 낮은 순서대로 빼내기
 
-typedef struct Data {
-    long long sum; //밑의 구간의 합 //최대 50억
-    int cnt; //밑의 구간에서 사용한 갯수
-} Data;
+typedef struct PriorityQueue {
+    int size;
+    int heap[MAXN];
+} PriorityQueue;
 
-Data segTree[4 * MAXN];
+void init(PriorityQueue *pq);
+int isEmpty(PriorityQueue *pq);
+void push(PriorityQueue *pq, int defensePower);
+int pop(PriorityQueue *pq);
+int peek(PriorityQueue *pq);
 
-void update(int node, int st, int ed, int pos, int value);
-int getMinCount(int node, int st, int ed, long long need, int values[]);
+void calculate(int A[MAXN], int needPower[MAXN], int idx, int P, PriorityQueue *pq);
 
-int cmpInt(const void *a, const void *b) {
-    int x = *(const int *)a;
-    int y = *(const int *)b;
-    if(x < y) return -1;
-    else if(x > y) return 1;
-    return 0;
-}
-
-int bSearch(int value, int cmpSize, int values[]);
+int prefixSum = 0;
 
 int main() {
     int N, P;
     scanf("%d %d", &N, &P);
     int A[MAXN];
-    //좌표 압축용 배열
-    int sorted[N];
     int needPower[MAXN];
     for(int i = 0; i < N; i++) {
         scanf("%d", &A[i]);
-        sorted[i] = A[i];
     }
 
-    qsort(sorted, N, sizeof(int), cmpInt);
-    int cmpSize = 1;
-    int values[MAXN];
-    values[0] = sorted[0];
-    for(int i = 1; i < N; i++) {
-        if(sorted[i] > values[cmpSize - 1]) values[cmpSize++] = sorted[i];
-    }
-    
+    PriorityQueue pq;
+    init(&pq);
+
     for(int i = 0; i < N; i++) {
-        //압축된 좌표로 바로 구간에 집어넣기
-        int compPos = bSearch(A[i], cmpSize, values);
-        update(1, 0, cmpSize - 1, compPos, A[i]);
-        //전체를 다 합쳐도 안됨
-        if(segTree[1].sum < P) needPower[i] = -1;
-        else needPower[i] = getMinCount(1, 0, cmpSize - 1, P, values);
+        calculate(A, needPower, i, P, &pq);
     }
 
     for(int i = 0; i < N; i++) {
@@ -68,40 +47,66 @@ int main() {
     return 0;
 }
 
-int bSearch(int targetValue, int cmpSize, int values[]) {
-    int l = 0;
-    int r = cmpSize;        
-    while(l < r) {
-        int mid = (l + r) >> 1;
-        if(values[mid] < targetValue) l = mid + 1;
-        else r = mid;
+//pq에서 오름차순으로 정렬된 허수아비들 중 제거할 수 있는 허수아비 부터 제거
+void calculate(int A[MAXN], int needPower[MAXN], int idx, int P, PriorityQueue *pq) {
+    prefixSum += A[idx];
+    push(pq, A[idx]);
+    while(!isEmpty(pq) && prefixSum - peek(pq) >= P) {
+        prefixSum -= pop(pq);
     }
-    return l;
+    needPower[idx] = prefixSum >= P ? pq -> size : -1;
 }
 
+//PQ 구현체
 
-void update(int node, int st, int ed, int pos, int value) {
-    if(st == ed) {
-        segTree[node].sum += value;
-        segTree[node].cnt += 1;
-        return;
-    }
-    int mid = (st + ed) >> 1;
-    if(pos <= mid) update(node * 2, st, mid, pos, value);
-    else update(node * 2 + 1, mid + 1, ed, pos, value);
-    segTree[node].sum = segTree[node * 2].sum + segTree[node * 2 + 1].sum;
-    segTree[node].cnt = segTree[node * 2].cnt + segTree[node * 2 + 1].cnt;
+void init(PriorityQueue *pq) {
+    pq -> size = 0;
 }
 
-int getMinCount(int node, int st, int ed, long long need, int values[]) {
-    if(st == ed) {
-        int v = values[st];
-        return (int) ((need + v - 1) / v); //올림;
+int isEmpty(PriorityQueue *pq) {
+    return pq -> size == 0;
+}
+
+void push(PriorityQueue *pq, int defensePower) {
+    int idx = ++pq -> size;
+    pq -> heap[idx] = defensePower;
+    //내림차순 PQ
+    while(idx > 1 && pq -> heap[idx] < pq -> heap[idx / 2]) {
+        int tmp = pq -> heap[idx];
+        pq -> heap[idx] = pq -> heap[idx / 2];
+        pq -> heap[idx / 2] = tmp;
+        idx /= 2;
     }
-    
-    int l = node * 2;
-    int r = node * 2 + 1;
-    int mid = (st + ed) >> 1;
-    if(segTree[r].sum >= need) return getMinCount(r, mid + 1, ed, need, values);
-    else return segTree[r].cnt + getMinCount(l, st, mid, need - segTree[r].sum, values);
+}
+
+int pop(PriorityQueue *pq) {
+    int result = pq -> heap[1];
+    pq -> heap[1] = pq -> heap[pq -> size--];
+    int idx = 1;
+    while(1) {
+        int left = idx * 2;
+        int right = idx * 2 + 1;
+        //현시점 최대값 찾아두는 것
+        int minimum = idx; 
+
+        if(left <= pq -> size && pq -> heap[left] < pq -> heap[minimum]) {
+            minimum = left;
+        }
+        if(right <= pq -> size && pq -> heap[right] < pq -> heap[minimum]) {
+            minimum = right;
+        }
+
+        if(minimum == idx) break;
+
+        int tmp = pq -> heap[idx];
+        pq -> heap[idx] = pq -> heap[minimum];
+        pq -> heap[minimum] = tmp;
+        
+        idx = minimum;
+    }
+    return result;
+}
+
+int peek(PriorityQueue *pq) {
+    return pq -> heap[1];
 }
